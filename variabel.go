8f306e21d1286feee6c7ce04fb88ae167b5b6bc6
zaki khairi <zakielbavarian@gmail.com>
2022-08-17 12:01:54 +0700

    init belajar-golang-dasar

---
+++ variabel.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// declare variabel with data types
+	var name string
+
+	name = "Zaki Khairi"
+	fmt.Println(name)
+
+	name = "Zaki Zwar"
+	fmt.Println(name)
+
+	// declare variabel with no data types
+	var friendName = "Karom"
+	fmt.Println(friendName)
+
+	var age = 30
+	fmt.Println(age)
+
+	// declare variabel with :=
+	country := "Indonesia"
+	fmt.Println(country)
+
+	country = "USA"
+
+	//declare multiple variabel
+	var (
+		firstname = "Zaki"
+		lastname  = "Khairi Ziwar"
+	)
+
+	fmt.Println(firstname, lastname)
+}
